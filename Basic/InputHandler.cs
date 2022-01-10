3448c57a631bd9f355aa0097b02d8a808ab615de
SubNet32 <97061134+SubNet32@users.noreply.github.com>
2022-01-10 17:58:59 +0100

    Add files via upload

---
+++ Basic/InputHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Basic
+{
+    public static class InputHandler
+    {
+        private static string path = "../InputFiles/day";
+        public static string[] GetInputForDay(int day)
+        {
+            string[] input = System.IO.File.ReadAllLines(path+day+".txt");
+            Console.WriteLine("Loaded " + input.Length + " items from inputFile");
+            return input;
+        }
+
+        public static int[] GetInputForDayInt(int day)
+        {
+            return Array.ConvertAll(GetInputForDay(day), s => Convert.ToInt32(s));
+        }
+    }
+}
